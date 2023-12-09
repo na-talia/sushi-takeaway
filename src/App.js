60ab3e90bdccf9832ed3590a1a2c19512253bf65
na-talia <ntsh_89@ukr.net>
2023-12-09 01:04:25 +0100

    added packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Promotions from "./components/Promotions";
 
 function App() {
   return (
     <div className="">
       <NavBar />
+      <Promotions />
     </div>
   );
 }
