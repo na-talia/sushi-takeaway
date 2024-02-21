34365a53762e7d035b9b33fee6c1463f6d446ff9
na-talia <ntsh_89@ukr.net>
2024-02-21 20:31:57 +0100

    adjust some styles

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -85,9 +85,9 @@ const mockReviews = [
   },
 ];
 
-const Review = ({ review }) => {
+function Review({ review }) {
   return (
-    <div className="flex-shrink-0 max-w-sm mx-4">
+    <div className="max-w-sm mx-4">
       <ul className="border border-gray-300 rounded p-4">
         <li>
           <div>{review.id}</div>
@@ -101,7 +101,7 @@ const Review = ({ review }) => {
       </ul>
     </div>
   );
-};
+}
 
 const Reviews = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -127,26 +127,28 @@ const Reviews = () => {
   );
 
   return (
-    <div className="text-white">
-      <Title text="ANMELDELSER FRA VORES KUNDE" />
-      <div className="flex overflow-x-auto">
-        {visibleReviews.map((review) => (
-          <Review key={review.id} review={review} />
-        ))}
-      </div>
-      <div className="flex justify-between mt-4">
-        <button
-          onClick={handlePrevClick}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNextClick}
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-        >
-          Next
-        </button>
+    <div className="text-white flex justify-center">
+      <div>
+        <Title text="ANMELDELSER FRA VORES KUNDE" />
+        <div className="flex overflow-x-auto mt-small">
+          {visibleReviews.map((review) => (
+            <Review key={review.id} review={review} />
+          ))}
+        </div>
+        <div className="flex justify-between mt-4">
+          <button
+            onClick={handlePrevClick}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextClick}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
